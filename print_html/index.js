2a27c854af94bb188e782b2bae4449668f52eb64
Puja Guchhait <138197906+Puja2004g@users.noreply.github.com>
2024-01-20 20:05:06 +0530

    Create index.js

---
+++ print_html/index.js
@@ -0,0 +1,11 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>This is a paragraph</p>
+    <h2>This is 2nd heading</h2>
+  </div>,
+  document.getElementById("root")
+);
