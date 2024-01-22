1cfb2ce062a3a5ce22ce4aaf1e5cfb9dd290576e
Puja Guchhait <138197906+Puja2004g@users.noreply.github.com>
2024-01-22 11:36:52 +0530

    Create math.jsx

---
+++ import-export/math.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const pi = 3.145;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function triplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { doublePi, triplePi };
